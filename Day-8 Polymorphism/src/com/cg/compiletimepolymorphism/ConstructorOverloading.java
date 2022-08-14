5b20d8a56685cb22b5b1a1c4741e5dbb07914ca0
HP <HP@192.168.0.105>
2022-08-14 10:24:41 +0530

    first commit

---
+++ Day-8 Polymorphism/src/com/cg/compiletimepolymorphism/ConstructorOverloading.java
@@ -0,0 +1,29 @@
+package com.cg.compiletimepolymorphism;
+
+class Triber
+{
+	int speed;
+	String engine;
+	Triber()
+	{
+		System.out.println("Renault-kiger");
+	}
+	//Parameterized
+	Triber(int speed,String engine)
+	{
+		this.speed=speed;
+		this.engine=engine;
+		 System.out.println("speed is :"+speed+" engine is : "+engine);
+	}
+}
+public class ConstructorOverloading {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Triber t1=new Triber();
+		@SuppressWarnings("unused")
+		Triber t2=new Triber(45,"Disel");
+
+	}
+
+}
