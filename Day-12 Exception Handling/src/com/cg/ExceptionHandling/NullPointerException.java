71f786e00ec51deec3e138c7819644e9f46661d7
HP <HP@192.168.0.102>
2022-08-18 12:40:34 +0530

    first commit

---
+++ Day-12 Exception Handling/src/com/cg/ExceptionHandling/NullPointerException.java
@@ -0,0 +1,14 @@
+package com.cg.ExceptionHandling;
+
+public class NullPointerException {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+	
+		String str=null;
+		str.equals("java");
+		
+
+	}
+
+}
---
+++ Day-12 Exception Handling/src/com/cg/ExceptionHandling/NullPointerExceptionHandling.java
@@ -0,0 +1,21 @@
+package com.cg.ExceptionHandling;
+
+public class NullPointerExceptionHandling {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+		
+		String str=null;
+		try
+		{
+			str.equals("java");
+		}
+		catch(Exception e) 
+		{
+			System.out.println("Exception Handle "+e);
+		}
+		
+
+	}
+
+}
